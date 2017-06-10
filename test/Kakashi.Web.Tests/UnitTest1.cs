3020d87c4f7cb50442f166e70384a69878a966b7
Nobuaki Yamaji <yamaji1012@gmail.com>
2017-06-11 04:01:24 +0900

    init

---
+++ test/Kakashi.Web.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kakashi.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
